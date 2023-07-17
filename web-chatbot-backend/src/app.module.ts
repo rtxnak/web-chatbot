e85a9ce321fcafad8a4aa433ef78844884f348e1
Rafael Nakashima <rafaelnakashima@rocketmail.com>
2023-07-17 17:22:14 -0300

    feat: added typeorm databases connection

--- web-chatbot-backend/src/app.module.ts
+++ web-chatbot-backend/src/app.module.ts
@@ -1,8 +1,26 @@
 import { Module } from '@nestjs/common';
-import { UsersModule } from './users/users.module';
+import { UserModule } from './users/user.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserEntity } from './users/entities/user.entity';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [UsersModule],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: process.env.ENV === 'test' ? '.test.env' : '.env',
+    }),
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.DB_HOST,
+      port: Number(process.env.DB_PORT),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
+      entities: [UserEntity],
+      synchronize: process.env.ENV === 'development',
+    }),
+    UserModule,
+  ],
   controllers: [],
   providers: [],
 })
---
+++ web-chatbot-backend/typeorm/database.providers.ts
@@ -0,0 +1,19 @@
+import * as dotenv from 'dotenv';
+import { DataSource } from 'typeorm';
+
+dotenv.config({
+  path: process.env.ENV === 'test' ? '.test.env' : '.env',
+});
+
+const dataSource = new DataSource({
+  type: 'mysql',
+  host: process.env.DB_HOST,
+  port: Number(process.env.DB_PORT),
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+  entities: [],
+  migrations: [`${__dirname}/migrations/**/*.ts`],
+});
+
+export default dataSource;
