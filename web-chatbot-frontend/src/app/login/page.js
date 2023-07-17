7b90d3dc77f2bc7491c7f996fcc1cb38a74069e0
Rafael Nakashima <rafaelnakashima@rocketmail.com>
2023-07-17 16:12:24 -0300

    feat: added login page

---
+++ web-chatbot-frontend/src/app/login/page.js
@@ -0,0 +1,66 @@
+'use client'
+
+import Link from 'next/link';
+import React, { useState } from 'react';
+
+const Login = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Lógica para autenticação do usuário
+  };
+
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-100">
+      <div className="max-w-md w-full px-6 py-8 bg-white rounded-lg shadow-md">
+        <h2 className="text-2xl font-bold mb-8 text-center">Login</h2>
+        <form onSubmit={handleSubmit}>
+          <div className="mb-4">
+            <label htmlFor="username" className="block mb-2 text-lg font-medium text-gray-700">
+              User
+            </label>
+            <input
+              type="text"
+              id="username"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+              className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500"
+              required
+            />
+          </div>
+          <div className="mb-6">
+            <label htmlFor="password" className="block mb-2 text-lg font-medium text-gray-700">
+              Password
+            </label>
+            <input
+              type="password"
+              id="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500"
+              required
+            />
+          </div>
+          <button
+            type="submit"
+            className="w-full bg-indigo-500 text-white py-3 rounded-md hover:bg-indigo-600 transition-colors duration-300"
+          >
+            Sign up
+          </button>
+        </form>
+        <div className="mt-4 text-center">
+          <p className="text-gray-700">
+            Don't have an account yet?{' '}
+            <Link href="/register" className="text-indigo-500 hover:text-indigo-600">
+              Register
+            </Link>
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
